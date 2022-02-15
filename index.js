4a5b10605729eb5bddabe4ea33460a16e1c79fc0
Parvathy Sajeev <parvathysajeev0@gmail.com>
2022-02-15 19:18:43 +0530

    db

--- index.js
+++ index.js
@@ -2,29 +2,25 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const { Client } = require('pg');
+// const { Pool } = require('pg');
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/db', (req, res) => {
-    
     var dbOpts = {
       connectionString: process.env.DATABASE_URL,
       ssl : true
     }
     const client = new Client(dbOpts);
-    client
-  .connect()
-  .then(() => console.log('connected'))
-  .catch(err => console.error('connection error', err.stack));
-    res.send('testing');
+    client.connect();
     client.query('SELECT id, name, Description__c FROM salesforce.example__c;', (err, dbRes) => {
           if (err) throw err;
           res.render('pages/db',{
             results : dbRes.rows
           });
-          //client.end();
+          client.end();
         });
     client.end();
   })
